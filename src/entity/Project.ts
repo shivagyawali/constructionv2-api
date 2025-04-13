27ad035d28c6446bf1cdb209dfd61c3c7d51b2ae
ShivaGyawali <shiv.alfa8@gmail.com>
2025-04-13 08:07:02 +0545

    added status column in project

--- src/entity/Project.ts
+++ src/entity/Project.ts
@@ -18,13 +18,20 @@ export class Project extends BaseEntity {
   @Column()
   name: string;
 
+  @Column({
+    type: "enum",
+    enum: ["PENDING", "INPROGRESS","ONHOLD", "COMPLETED"],
+    default: "PENDING",
+  })
+  status: "PENDING"| "INPROGRESS"|"ONHOLD"| "COMPLETED";
+
   @Column({ type: "text", nullable: true })
   description: string;
 
   @OneToMany(() => Task, (task) => task.project)
   tasks: Task[];
-  
+
   @ManyToOne(() => Company, (company) => company.projects)
-  @JoinColumn({ name: "companyId" }) 
+  @JoinColumn({ name: "companyId" })
   company: Company;
 }
